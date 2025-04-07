677117d75625e3bc823307c114ab0ac8f2d723b7
Salma Nur Azizah <azizahsalma2304@gmail.com>
2025-04-07 21:27:03 +0700

    Update splash screen

--- app/src/main/java/com/salmaazizah0040/hydrocalc/MainActivity.kt
+++ app/src/main/java/com/salmaazizah0040/hydrocalc/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.salmaazizah0040.hydrocalc.ui.screen.MainScreen
+import com.salmaazizah0040.hydrocalc.ui.screen.App
 import com.salmaazizah0040.hydrocalc.ui.theme.HydroCalcTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
-        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             HydroCalcTheme {
-                MainScreen()
+                App()
             }
         }
     }
